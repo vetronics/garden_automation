8b0e0a620a2f6df07e89f4ae5ff34dc80101c82d
vetronics <system_solutions@etik.com>
2025-06-13 10:09:29 +0200

    Add files via upload

---
+++ garden_autoamtion.ino
@@ -0,0 +1,107 @@
+// Import required libraries
+#include <Ultrasonic.h>
+#include <LibPrintf.h>
+
+// Define infrared sensor pin
+
+#define IR 3
+
+// define pin water level  analog input pin
+
+#define WATER_LEVEL A0
+
+// Define temperature sensor analog input pins
+
+#define temp1 A1
+#define temp2 A5
+#define temp3 A3
+
+// Define pins for LED and buzzer alerts
+
+#define yellow_led 8
+#define BUZZER 12
+
+// Define relay output pins
+#define rele1 4
+#define rele2 6
+#define rele3 7
+
+// Initialize ultrasonic sensor (Trigger: pin 11, Echo: pin 10)
+
+Ultrasonic ultrasonic(11, 10);
+
+void setup() {
+
+  Serial.begin(9600);  // Initialize serial communication
+
+  // Configure output pins for alerts and relays
+
+  pinMode(yellow_led, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(rele1, OUTPUT);
+  pinMode(rele2, OUTPUT);
+  pinMode(rele3, OUTPUT);
+
+  // Configure IR sensor pin as input
+
+  pinMode(IR, INPUT);
+
+  // Set up interrupt to disable relays when the IR sensor is triggered
+
+  attachInterrupt(digitalPinToInterrupt(IR), disable_reles, RISING);
+}
+
+void loop() {
+
+// water level by analog sensor 
+
+  int unsigned level = analogRead(WATER_LEVEL);
+
+  // Map raw sensor values to a 0–100 scale
+
+  int unsigned level_update = map(level, 0, 1023, 0, 100);
+
+  // Read raw values from temperature sensors
+
+  unsigned int sensor1 = analogRead(temp1);
+  unsigned int sensor2 = analogRead(temp2);
+  unsigned int sensor3 = analogRead(temp3);
+
+  // Map raw sensor values to a 0–100 scale
+
+  unsigned int temp_map = map(sensor1, 0, 1023, 0, 100);
+  unsigned int temp_map_2 = map(sensor2, 0, 1023, 0, 100);
+  unsigned int temp_map_3 = map(sensor3, 0, 1023, 0, 100);
+
+  // Read distance from ultrasonic sensor
+
+  int distance = ultrasonic.read();
+
+  // Calculate time-of-flight (ToF) in microseconds
+
+  float tof = distance * 58;
+
+  // Debug procedure (manually activate if needed)
+  //debug_sensors(temp_map, temp_map_2, temp_map_3, distance,level_update);
+
+  // Condition: bucket is full
+
+  if (distance > 0 && distance < 12 && tof > 0.3) {
+
+    Serial.println("Bucket is full");
+
+    // Call relay control lambda
+
+    auto rele_control(temp_map, temp_map_2, temp_map_3);
+
+  } else {
+
+    // Condition: bucket is empty
+
+    disable_reles();  // Disable all relays
+
+    delay(5);  // Small delay before alert
+
+    bucket_is_empty();  // Trigger alert procedure
+  }
+}
---
+++ procedures_and_functions.ino
@@ -0,0 +1,113 @@
+/*
+  Debug flag:
+  Set to 0 to disable debug output.
+  Set to 1 to enable debug output and uncomment the call to debug_sensors()
+  in garden_automation.ino (line 68).
+*/
+
+#define DEBUG_LEVEL 0
+
+#if DEBUG_LEVEL > 0
+
+// Debug procedure: prints sensor values with controlled delay for monitoring
+
+void debug_sensors(unsigned int &temp_map, unsigned int &temp_map_2, unsigned int &temp_map_3, int &distance, int unsigned &level_update) {
+
+  printf("Water level by ultrasonic tof:\n");
+
+  delay(100);
+
+  Serial.println(distance);
+
+  printf("Temp sensor 1:\n");
+
+  delay(200);
+
+  Serial.println(temp_map);
+
+  printf("Temp sensor 2:\n");
+
+  delay(300);
+
+  Serial.println(temp_map_2);
+
+  printf("Temp sensor 3:\n");
+
+  delay(400);
+
+  Serial.println(temp_map_3);
+
+  printf("water level \n");
+
+  delay(200);
+
+  Serial.println(level_update);
+}
+
+#endif
+
+// Procedure (lambda) to control relays based on temperature
+
+auto rele_control = [](unsigned int &temp_map, unsigned int &temp_map_2, unsigned int &temp_map_3) {
+
+  // millis variables
+  
+  int unsigned long t1, t2;
+
+  t1 = millis();
+
+  t2 = 0;
+
+  if (temp_map > 0 && temp_map < 100 && t1 - t2 > 100 && t1 - t2 < 200) {
+
+
+    digitalWrite(rele1, LOW);
+
+  } else if (temp_map_2 > 0 && temp_map_2 < 100 && t1 - t2 > 200 && t1 - t2 < 300) {
+
+
+    digitalWrite(rele2, LOW);
+
+  } else if (temp_map_3 > 300 && temp_map_3 < 400) {
+
+
+    digitalWrite(rele3, LOW);
+  }
+
+  digitalWrite(rele1, HIGH);
+  digitalWrite(rele2, HIGH);
+  digitalWrite(rele3, HIGH);
+
+  t1 = t2;
+};
+
+// Procedure to alert the user when the bucket is empty using buzzer and LED
+
+void bucket_is_empty() {
+
+  // couter for loop
+
+  int j;
+
+  for (j = 8000; j > 0; j = j - 1000) {
+
+    tone(BUZZER, j);  // Emit tone at decreasing frequencies
+
+    delay(100);  // Tone duration
+
+    noTone(BUZZER);  // Stop tone
+
+    digitalWrite(yellow_led, !digitalRead(yellow_led));  // Toggle LED
+
+    delay(500);  // Delay between blinks
+  }
+}
+
+// Procedure to disable all relays (set output HIGH)
+
+void disable_reles() {
+
+  digitalWrite(rele1, HIGH);
+  digitalWrite(rele2, HIGH);
+  digitalWrite(rele3, HIGH);
+}
